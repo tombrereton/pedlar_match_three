c1b4253600e6608a9c1de53d00ac89b127a99214
Tom Brereton <tombrereton@Toms-MacBook-Pro.local>
2022-10-19 19:00:36 +1100

    add column collapse

--- Scripts/piece.gd
+++ Scripts/piece.gd
@@ -10,7 +10,7 @@ func _ready():
 
 func move(target):
 	move_tween.interpolate_property(self, "position", position, target, speed, 
-							Tween.TRANS_SINE, Tween.EASE_OUT)
+							Tween.TRANS_BOUNCE, Tween.EASE_OUT)
 	move_tween.start()
 
 func dim():
